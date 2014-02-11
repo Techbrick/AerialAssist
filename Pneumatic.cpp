61c4d12d318e48b0dc542eda55825b88d2e13791
Numeri <kadenu1@gmail.com>
2014-02-10 22:13:16 -0500

    Wrote Pneumatic.cpp

---
+++ Pneumatic.cpp
@@ -0,0 +1,33 @@
+#include "WPILib.h"
+
+class Pneumatic {
+	Solenoid InSole;
+	Solenoid OutSole;
+public:
+	Pneumatic (UINT32 InSoleChannel, UINT32 OutSoleChannel):
+		InSole (InSoleChannel),
+		OutSole (OutSoleChannel)
+	{    }
+
+	void Set (bool state);	// true is out, false is in
+	bool Get ();			// the same
+};
+
+void Pneumatic::Set (bool state)
+{
+	if (InSole.Get())
+	{
+		InSole.Set (!state);
+		OutSole.Set (state);
+	}
+	else
+	{
+		OutSole.Set (state);
+		InSole.Set (!state);
+	}
+}
+
+bool Pneumatic::Get ()
+{
+	return OutSole.Get();
+}
