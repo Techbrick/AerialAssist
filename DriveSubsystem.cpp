4e81ef0b3b689856f8722d80348ed4342a5de9d7
Numeri <kadenu1@gmail.com>
2014-02-12 19:06:33 -0500

    Added Super Shifter code

--- DriveSubsystem.cpp
+++ DriveSubsystem.cpp
@@ -1,19 +1,35 @@
 #include "WPILib.h"
-// Specific path for Numeri: ..\..\Techbrick\MainWorkingCode\
-// WPILib\WPILib\WPILib.h
+#include "Pneumatic.cpp"
 
 /* This is a simple wrapper for RobotDrive.
- * If needed, we could add more functions to it.
- * (Possibly a Talon current threshold function to prevent
- * burning them out)
+ * Includes TankDrive w/ Super Shifters
  */
 
+//TODO: Possibly a Talon current check to prevent burnouts.
+
 class DriveSubsystem : public RobotDrive {
+
+	Pneumatic LeftSuper;	//Left Super Shifter
+	Pneumatic RightSuper;	//Right - -
 
 public:
 	DriveSubsystem (UINT32 frontLeft, UINT32 rearLeft, UINT32 frontRight,
 		UINT32 rearRight) : RobotDrive(frontLeft, rearLeft, frontRight, rearRight)
-	{
+	{    }
 
+	void TankDrive(GenericHID &leftStick, GenericHID &rightStick)
+	{
+		TankDrive(leftStick.GetY(), rightStick.GetY());
+
+		if (leftStick.GetButton(1))
+		{
+			LeftSuper.Set(true);	//Push out both shifters 
+			RightSuper.Set(true);
+		}
+		else
+		{
+			LeftSuper.Set(false);	//Pull in both shifters 
+			RightSuper.Set(false);
+		}
 	}
 };
