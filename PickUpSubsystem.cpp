877f9c3a91450d649e375fc9980ddff40a684203
mitchellroberts2015 <mitchellroberts2015@gmail.com>
2014-02-15 16:38:46 -0500

    Pick up code

--- PickUpSubsystem.cpp
+++ PickUpSubsystem.cpp
@@ -1,18 +1,39 @@
 #include "WPILib.h"			// the library
 #include "PinDefinitions.h" // holds all the pin numbers
+#include "Pneumatic.cpp"
 // actual path of the library: wpilib/WPILib.h
 // press ctrl+shift+o when the cursor is on ^ to view the file
 
 class PickUpSubsystem {
-	//here's your initialization code
-	Jaguar jag1;
+	Relay rollerSpike;
+	Pneumatic pickupArm;
 	
-	//methods
 public:
 	PickUpSubsystem (void) :
-		// same order as above
-		jag1(EXAMPLE_PIN)
+		rollerSpike(PICKUP_ROLLERSPIKE),
+		pickupArm(PICKUP_PICKUPARMSOLIN, PICKUPARMSOLOUT)		
+	{	}
+	
+	void SetArm(bool setting)
 	{
+		pickupArm.Set(setting);
+	}
+	
+	void SetRoller(int direction)
+	{
+		if (direction == -1)
+		{
+			rollerSpike.Set(Relay::kBackwards);
+		}
 		
+		if (direction == 0)
+		{
+			rollerSpike.Set(Relay::kOff);
+		}
+		
+		if (direction == 1)
+		{
+			rollerSpike.Set(Relay::kOn);
+		}
 	}
 };
