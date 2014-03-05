9343ee4d5e293c6333ce0cf349f5a35595019780
0queue <ta.harris.42@gmail.com>
2014-03-04 20:22:32 -0500

    Added line locking code

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -23,6 +23,8 @@ class RobotDemo : public SimpleRobot
         Joystick rightStick;
         Joystick operatorStick;
         Compressor compressor;
+        DigitalInput lpswitch;
+        DigitalInput rpswitch;
         NetworkListener l;
 
 public:
@@ -34,6 +36,8 @@ public:
                 rightStick(1),
                 operatorStick(3),
                 compressor(PRESSURESWITCH_PIN, COMPRELAY_PIN),
+                lpswitch(DRIVE_LEFTPSWITCH),
+                rpswitch(DRIVE_RIGHTPSWITCH),
                 l()
 	{
 		myRobot.SetExpiration(0.1);
@@ -44,6 +48,26 @@ public:
                 myRobot.SetInvertedMotor(RobotDrive::kRearRightMotor, true);
 	}
 
+        void LockToLine ()
+        {
+            // make sure value is correct
+
+            while (lpswitch.Get() == 0 || rpswitch.Get() == 0) { // drive until switch is triggered
+                myRobot.TankDrive(.3, .3);
+            }
+
+            if (lpswitch.Get() != 0) { // if it hit the left switch first
+                while (rpswitch.Get() == 0) {
+                    myRobot.TankDrive(0, .3);
+                }
+            } else if (rpswitch.Get() != 0) { // if it hit the right switch first
+                while (lpswitch.Get() == 0) {
+                    myRobot.TankDrive(.3, 0);
+                }
+            }
+
+            // we are now on the line
+        }
 
 	///////////////////////////////////////////////////////////////
 	void Autonomous(void)
@@ -80,8 +104,8 @@ public:
 
                         // shooting controls
                         if (operatorStick.GetRawButton(8)) {
-                            shooter.Prime();
-                        } else if (oepratorStick.GetRawButton(1)) {
+                            shooter.Prime(&operatorStick, SHOOTER_CANCELBUTTON);
+                        } else if (operatorStick.GetRawButton(1)) {
                             shooter.Fire();
                         } else if (operatorStick.GetRawButton(11)) {
                             shooter.Cancel(); // doesn't actually work for some reason
