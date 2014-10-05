fd6c8afa54b7f78292bf576727bb072ad3059f34
mitchellroberts2015 <mitchellroberts2015@gmail.com>
2014-10-05 19:36:50 -0400

    Fixed double pointer in cancel
    Cancel tried to point to a pointer to a joystick, but only dereferenced
    once. Got rid of ampersand in task start. Got rid of potentiometer and a
    few other broken functions.

--- ShooterSubsystem.cpp
+++ ShooterSubsystem.cpp
@@ -20,21 +20,24 @@ inline void primeTaskFunc(UINT32 motorLockPistonPtr, UINT32 ratchetPistonPtr, UI
 	Relay *winch = (Relay *) winchPtr;
 	DigitalInput *winchLimitSwitch = (DigitalInput *) winchLimitSwitchPtr;
 	Joystick *joystickCancel = (Joystick *) joystickCancelPtr;
-
-
+	
 	ratchetPiston->Set(true);	// This order is important
 	motorLockPiston->Set(true);
-
+	Wait(.25);
 	winch->Set(Relay::kReverse);
 
 	//while the winch hasn't hit the limit.
-	while ( winchLimitSwitch->Get() == false && !joystickCancel->GetRawButton(joystickButtonInt))
+	while ( winchLimitSwitch->Get() == false && joystickCancel->GetRawButton(9) == false)
 	{
-
+		SmartDashboard::PutBoolean("WINCH SWITCH DEBUG", winchLimitSwitch->Get());
+		//printf("in loop \n");
 	}
-
+	SmartDashboard::PutBoolean("WINCH SWITCH DEBUG", winchLimitSwitch->Get());
+	printf("out of loop \n");
 	winch->Set(Relay::kOff);
+	Wait(.5);
 	motorLockPiston->Set(false);
+  Wait(.25);
 }
 
 
@@ -48,27 +51,62 @@ class Shooter {
 	Talon arm;
 	DigitalInput armBackLimitSwitch;
 	DigitalInput armFrontLimitSwitch;
-	Potentiometer armPot;
+	//Potentiometer armPot;
 
 	Task primeTask;
 	
 public:
 	Shooter () :
-		winch (SHOOTER_WINCHSPIKE),
+		winch (SHOOTER_WINCH_SPIKE),
 		winchLimitSwitch (SHOOTER_WINCHLIMITSWITCH),
 		motorLockPiston (SHOOTER_WINCHMOTORSOLIN, SHOOTER_WINCHMOTORSOLOUT),
 		ratchetPiston (SHOOTER_WINCHRATCHSOLIN, SHOOTER_WINCHRATCHSOLOUT),
 		arm (SHOOTER_ARMTALON),
 		armBackLimitSwitch (SHOOTER_ARMLIMITSWITCHBACK),
 		armFrontLimitSwitch (SHOOTER_ARMLIMITSWITCHFRONT),
-		armPot (SHOOTER_ARMPOT),
+		//armPot (SHOOTER_ARMPOT),
 		primeTask ("Prime", (FUNCPTR) primeTaskFunc)
 	{    }
 
 	void Prime(Joystick *joystickCancel, UINT32 joystickCancelButton)
 	{
-		primeTask.Start((UINT32) &motorLockPiston, (UINT32) &ratchetPiston, (UINT32) &winch, (UINT32) &winchLimitSwitch, (UINT32) &joystickCancel, joystickCancelButton);
+		primeTask.Start((UINT32) &motorLockPiston, (UINT32) &ratchetPiston, (UINT32) &winch, (UINT32) &winchLimitSwitch, (UINT32) joystickCancel, joystickCancelButton);
 	}
+
+void SetRatchet (bool state) {
+		ratchetPiston.Set(state);
+	}
+	
+	/*void moveTo(int position) {
+		// back is low
+		// forward is high
+		int sign;
+		double targetV;
+		if (position == ANGLE_AUTO) {
+			targetV = 3.83;
+			sign = ((GetVoltage() - targetV) < 0) ? 1 : -1;
+		}
+		
+		if (sign > 0) { // need to move forwards
+			while (GetVoltage() < targetV) {
+				MoveArm(sign*.4);
+				//WatchdogFeed();
+				Wait(0.01);
+			}
+		} else if (sign < 0) {
+			while (GetVoltage() > targetV) {
+				MoveArm(sign*.4);
+				Wait(.01);
+			}
+		}
+		
+		MoveArm(0);
+		
+	}*/
+	
+	/*double GetVoltage () {
+		return armPot.GetVoltage();
+	}*/
 
 	bool Cancel()
 	{
@@ -77,23 +115,26 @@ public:
 
 	void Fire()
 	{
-		while (primeTask.GetID() + 1)
+		if (primeTask.GetID() + 1)
 		{
-
+			return;
 		}
 
 		ratchetPiston.Set(false);	// fire.
 		Wait(0.1);
 	}
 
-	float GetAngle()
+	/*float GetAngle()
 	{
 		return armPot.Get()*160.0 + 20.0;
-	}
+	}*/
 
 	void MoveArm(float power)
 	{		
-		if ((armBackLimitSwitch.Get() && power<0.0) || (armFrontLimitSwitch.Get() && power>0.0))
+		SmartDashboard::PutBoolean("BACK SWITCH", armBackLimitSwitch.Get());
+		SmartDashboard::PutBoolean("FRONT SWITCH", armFrontLimitSwitch.Get());
+		SmartDashboard::PutBoolean("WINCH SWITCH 2", winchLimitSwitch.Get());
+    if ((armBackLimitSwitch.Get() && power<0.0) || (armFrontLimitSwitch.Get() && power>0.0))
 		{
 			arm.Set(0);
 		}
@@ -103,7 +144,7 @@ public:
 		}
 	}
 
-	void Calibrate()
+	/*void Calibrate()
 	{
 		arm.Set(0.8);	//TODO: make sure this is the right direction!
 
@@ -120,5 +161,5 @@ public:
 
 		arm.Set(0.0);
 		armPot.CalibrateEnd();
-	}
+	}*/
 };
